51f4ae2f2580d5440ba92bfbaa5dd1c84abd6082
eduprivate <edu.private@gmail.com>
2023-11-09 23:18:46 -0300

    add filter

---
+++ src/main/java/com/edu/salem/model/Filter.java
@@ -0,0 +1,28 @@
+package com.edu.salem.model;
+
+public class Filter {
+    private String name;
+    private String type;
+
+    public Filter(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
--- src/main/java/com/edu/salem/model/SearchResponseModel.java
+++ src/main/java/com/edu/salem/model/SearchResponseModel.java
@@ -2,12 +2,18 @@ package com.edu.salem.model;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Map;
 
 public class SearchResponseModel implements Serializable {
     private List<Product> products;
+    private Map<Filter, String> filters;
 
     public SearchResponseModel(List<Product> products) {
         this.products = products;
+    }
+
+    public SearchResponseModel(Map<Filter, String> filters) {
+        this.filters = filters;
     }
 
     public List<Product> getProducts() {
@@ -16,5 +22,13 @@ public class SearchResponseModel implements Serializable {
 
     public void setProducts(List<Product> products) {
         this.products = products;
+    }
+
+    public Map<Filter, String> getFilters() {
+        return filters;
+    }
+
+    public void setFilters(Map<Filter, String> filters) {
+        this.filters = filters;
     }
 }
