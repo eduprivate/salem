d4f7ea80283f7d8ae11dab7e30b5037c0f4e27d6
eduprivate <edu.private@gmail.com>
2024-02-21 17:18:55 -0300

    unused import

--- src/main/java/com/edu/salem/controller/SearchController.java
+++ src/main/java/com/edu/salem/controller/SearchController.java
@@ -2,17 +2,15 @@ package com.edu.salem.controller;
 
 import com.edu.salem.model.ComplexQueryRequestModel;
 import com.edu.salem.model.SearchResponseModel;
+import com.edu.salem.service.SearchService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.edu.salem.service.SearchService;
-
 import java.io.IOException;
 import java.util.Optional;
-import java.util.logging.Level;
 
 @RestController
 public class SearchController {
